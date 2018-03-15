21e4287fac9e6be97ce4321267c049e4bbd27978
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-03-15 11:52:28 +0800

    终于搞懂了跳转

--- app/screens/createpage/MainCreate.js
+++ app/screens/createpage/MainCreate.js
@@ -1,20 +1,78 @@
 /**
+ * 之前报的navigation未定义的错误，应该是我的click方法没有绑定的原因，没有用()=>，也没有用bind
+ * 参考http://bbs.reactnative.cn/topic/2480/bind%E5%92%8C%E7%AE%AD%E5%A4%B4%E5%87%BD%E6%95%B0-%E5%93%AA%E4%B8%AA%E6%9B%B4%E5%A5%BD%E5%91%A2/2
  * Description:
  */
 import React, {Component} from 'react';
 import {
     StyleSheet,
     View,
+    TouchableOpacity,
     Text,
 } from 'react-native';
 
 export default class MainCreate extends Component {
+    //----------------------跳转，方法1：:-------------------------------------
+    // render() {
+    //     return (
+    //         <View style={styles.container}>
+    //                 <TouchableOpacity activeOpacity={1} onPress={()=>this._clickItem('TextPage')}>
+    //                 <Text style={styles.text}>MainCreate</Text>
+    //             </TouchableOpacity>
+    //         </View>
+    //     );
+    // }
+    //
+    // _clickItem(screen){
+    //     console.log("test")
+    //     const { navigate } = this.props.navigation;
+    //     navigate(screen);
+    // }
+
+//--------------------------跳转，方法2：---------------------------------
+    // render() {
+    //     const { navigate } = this.props.navigation;
+    //     return (
+    //         <View style={styles.container}>
+    //             <TouchableOpacity activeOpacity={1} onPress={this._clickItem.bind(this,navigate)}>
+    //                 {/*<TouchableOpacity activeOpacity={1} onPress={()=>this.props.navigation.navigate('TextPage')}>*/}
+    //                 <Text style={styles.text}>MainCreate</Text>
+    //             </TouchableOpacity>
+    //         </View>
+    //     );
+    // }
+    //
+    // _clickItem(navigate){
+    //     console.log("test")
+    //     navigate('TextPage');
+    // }
+
+//---------------------------跳转，方法3：--------------------------------
+    // render() {
+    //     return (
+    //         <View style={styles.container}>
+    //                 <TouchableOpacity activeOpacity={1} onPress={()=>this.props.navigation.navigate('TextPage')}>
+    //                 <Text style={styles.text}>MainCreate</Text>
+    //             </TouchableOpacity>
+    //         </View>
+    //     );
+    // }
+
+//--------------------------跳转，方法4：---------------------------------
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>MainCreate</Text>
+                <TouchableOpacity activeOpacity={1} onPress={()=>this._clickItem()}>
+                    <Text style={styles.text}>MainCreate</Text>
+                </TouchableOpacity>
             </View>
         );
+    }
+
+    _clickItem(){
+        console.log("test")
+        const { navigate } = this.props.navigation;
+        navigate('TextPage');
     }
 }
 
@@ -26,7 +84,7 @@ const styles = StyleSheet.create({
         alignItems: 'stretch',//定制副轴
         alignContent: 'flex-start',
         backgroundColor: '#f2f2f2',
-    },text:{
+    }, text: {
         fontSize: 50,
         textAlign: 'center',
         color: '#333333',
