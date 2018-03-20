71122fc3d872fd3aed5aa5e444e9e639120431a1
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-03-20 10:22:34 +0800

    scrollview demo

--- app/screens/demopage/widgets/ScrollViewDemo.js
+++ app/screens/demopage/widgets/ScrollViewDemo.js
@@ -13,7 +13,7 @@ import {
     TouchableOpacity,
     View,
     ScrollView,
-    TextInput, Dimensions
+    Text, Dimensions
 } from 'react-native'
 
 export default class ScrollViewDemo extends Component {
@@ -41,7 +41,7 @@ export default class ScrollViewDemo extends Component {
         // 数组
         var itemAry = [];
         // 颜色数组
-        var colorAry = [ 'green','blue', 'yellow', 'black', 'orange', 'white', 'pink'];
+        var colorAry = ['green', 'blue', 'yellow', 'black', 'orange', 'white', 'pink'];
         // 遍历
         for (var i = 0; i < colorAry.length; i++) {
             itemAry.push(
@@ -51,24 +51,46 @@ export default class ScrollViewDemo extends Component {
         return itemAry;
     }
 
+    //回到顶部
+    _toBeTop(){
+        alert("回到顶部")
+    }
+    _scroll;
     render() {
         return (
             //contentContainerStyle 这些样式会应用到一个内层的内容容器上，所有的子视图都会包裹在内容容器内
             <ScrollView
+                //在react-native中可以通过 ref属性来获取组件，并设置组件的属性及其方法，相当于android中为控件指定id的感觉
+                ref={(scroll)=>this._scroll = scroll}
                 scrollsToTop={true}//ios 当此值为true时，点击状态栏的时候视图会滚动到顶部。默认值为true。
-                contentContainerStyle={styles.container}
+                contentContainerStyle={styles.container}>
 
-                onScroll={(event) => {
-                    console.log(event.nativeEvent.contevtOffset.x);//水平滚动距离
-                    console.log(event.nativeEvent.contevtOffset.y);//垂直滚动距离
-                }}
-            >
+                <TouchableOpacity
+                    style={styles.view_top}
+                    activeOpacity={0.5}
+                    //滚动到末尾
+                    onPress={()=>this._scroll.scrollToEnd()}>
+                    <Text style={styles.view_txt}>
+                        滚动到底部
+                    </Text>
+                </TouchableOpacity>
+
                 <ScrollView
                     style={styles.scrollViewStyle}
                     horizontal={true}>
                     {this.renderItemH()}
                 </ScrollView>
                 {this.renderItemV()}
+
+                <TouchableOpacity
+                    style={styles.view_top}
+                    activeOpacity={0.5}
+                    //滚动到指定位置
+                    onPress={()=>this._scroll.scrollTo({x:0,y:0,animated:true})}>
+                        <Text style={styles.view_txt}>
+                            回到顶部
+                        </Text>
+                </TouchableOpacity>
             </ScrollView>
         );
     }
@@ -81,5 +103,14 @@ const styles = StyleSheet.create({
         // 尺寸
         width: 1000,
         height: 200
-    },
+    }, view_top: {
+        flex: 1,
+        backgroundColor: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }, view_txt: {
+        color: 'white',
+        fontSize: 20,
+        padding: 20
+    }
 });
